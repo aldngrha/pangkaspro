1f38dff96267c73ff16f78772da54c41468848bc
aldngrha <aldi.nugrahatk@gmail.com>
2023-04-05 14:39:33 +0700

    add footer section

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="p-5 border-t-2 -mr-4 -ml-4 border-gray-100">
+      <div className="flex justify-center items-center">
+        <p className="font-nunito font-light text-gray-500">
+          Copyright All Right Reserved PangkasPro
+        </p>
+      </div>
+    </footer>
+  );
+}
